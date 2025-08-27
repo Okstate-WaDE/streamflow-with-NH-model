ab9fa8a035fd9b9f0f058bdbbdbe83a4d9f3db2b
charlesliu0506 <charlesliu0506@gmail.com>
2025-08-27 11:41:06 -0500

    pre-loaded GENERIC with basins 01031500 and 07328500, finished documenting setup_gridmet.py

--- GENERIC/setup_gridmet.py
+++ GENERIC/setup_gridmet.py
@@ -5,7 +5,7 @@ This Python script was used to set up GENERIC, using open-soruce data from onlin
 """
 
 __author__ = "Charles Liu"
-__date__ = "2025-08-22"
+__date__ = "2025-08-27"
 
 from pathlib import Path
 from typing import Tuple
@@ -16,6 +16,7 @@ import statistics
 import numpy as np
 import pandas as pd
 import pygridmet as gridmet
+from shapely import Geometry
 from pynhd import NLDI
 
 # This path assumes that this file, setup_gridmet.py, is located directly in GENERIC.
@@ -27,7 +28,8 @@ NLDI_DB = NLDI()
 
 def calc_seasonality(attr : pd.DataFrame) -> float:
     """
-    Calculates seasonality (a metric for when during the year it's rainiest) from NLDI attributes.
+    Calculates seasonality (a metric for when during the year it's rainiest)
+    from NLDI attributes. Helper function for load_attributes.
 
     Parameters
     ----------
@@ -58,7 +60,7 @@ def calc_seasonality(attr : pd.DataFrame) -> float:
 
 def calc_soil_stats(attr : pd.DataFrame, bias : float, weights : Tuple[float, float, float, float, float, float, float]) -> float:
     """
-    Does multiple linear regression (using provided weights and bias) on NLDI attributes.
+    Does multiple linear regression (using provided weights and bias) on NLDI attributes. Helper function for load_attributes.
 
     Parameters
     ----------
@@ -81,8 +83,8 @@ def calc_soil_stats(attr : pd.DataFrame, bias : float, weights : Tuple[float, fl
 
 def calc_prec_stats(df : pd.DataFrame, p_mean : float) -> Tuple[list, list, list, list]:
     """
-    Calculates precipitation-related attributes high_prec_freq, high_prec_dur,
-    low_prec_freq, and low_prec_dur from meteorological forcings.
+    Calculates precipitation-related attributes high_prec_freq, high_prec_dur, low_prec_freq,
+    and low_prec_dur from meteorological forcings. Helper function for load_attributes.
 
     Parameters
     ----------
@@ -131,8 +133,25 @@ def calc_prec_stats(df : pd.DataFrame, p_mean : float) -> Tuple[list, list, list
             [365 * low_counter/len(df["prcp"])], [statistics.fmean(low_events)])
 
 def load_attributes(gauge_id : str, df : pd.DataFrame) -> pd.DataFrame:
+    """
+    Loads the attributes of a basin based on forcings, NLDI data, and calculations.
+
+    Parameters
+    ----------
+    gauge_id : str
+        The USGS gauge ID of the desired basin.
+    df : pd.DataFrame
+        A Pandas DataFrame containing daily precipitation data.
+    
+    Returns
+    -------
+    pd.DataFrame
+        A Pandas DataFrame containing labeled and adjusted (to match CAMELS) attributes.
+    """
+    # Get the COM ID and setup data dict to eventually turn into DataFrame.
     com_id = int(NLDI_DB.getfeature_byid("nwissite", f"USGS-{gauge_id}")["comid"].iloc[0])
     data = {}
+    # These are the characteristics from NLDI that we will turn into attributes.
     attr = NLDI_DB.get_characteristics(["TOT_SANDAVE", "TOT_SILTAVE", "TOT_CLAYAVE", "TOT_PERMAVE",
                                      "TOT_PPT7100_JAN", "TOT_PPT7100_FEB", "TOT_PPT7100_MAR", "TOT_PPT7100_APR",
                                      "TOT_PPT7100_MAY", "TOT_PPT7100_JUN", "TOT_PPT7100_JUL", "TOT_PPT7100_AUG",
@@ -142,13 +161,14 @@ def load_attributes(gauge_id : str, df : pd.DataFrame) -> pd.DataFrame:
     data["p_mean"] = [df["prcp"].mean()]
     data["pet_mean"] = [df["pet"].mean()]
     data["aridity"] = [data["pet_mean"][0] / data["p_mean"][0]]
-    data["p_seasonality"] = [calc_seasonality(attr)] # multi regression
+    data["p_seasonality"] = [calc_seasonality(attr)] # seasonality is calculated through multiple regression of monthly prcp
     data["frac_snow"] = [df["snow"].mean() / data["p_mean"][0]]
     data["high_prec_freq"], data["high_prec_dur"], data["low_prec_freq"], data["low_prec_dur"] = calc_prec_stats(df, data["p_mean"][0])
     data["elev_mean"] = list(attr["TOT_ELEV_MEAN"])
-    data["slope_mean"] = [2.76435 * attr["TOT_BASIN_SLOPE"].iloc[0] - 3.20257] # linear regression
+    data["slope_mean"] = [2.76435 * attr["TOT_BASIN_SLOPE"].iloc[0] - 3.20257] # unsure why but CAMELS and NLDI have very different slopes
     data["area_gages2"] = list(attr["TOT_BASIN_AREA"])
-    data["geol_permeability"] = [np.log10(attr["TOT_PERMAVE"].iloc[0]) - 15]
+    # Soil statistics also vary drastically between NLDI and CAMELS, convert most through multiple regression.
+    data["geol_permeability"] = [np.log10(attr["TOT_PERMAVE"].iloc[0]) - 15] # logarithm and convert units
     data["soil_conductivity"] = [calc_soil_stats(attr, -798.9465898, (8.030292344, 7.968451358, 7.974683175, 0.177809034, 0.003841075, 6.14001913, -0.000990142))]
     data["soil_depth_pelletier"] = [calc_soil_stats(attr, -3152.809045, (31.10022842, 30.9461941, 31.15743186, 1.566249831, 0.550005918, 202.6447689, 0.135290205))]
     data["soil_depth_statsgo"] = [calc_soil_stats(attr, 2.308989753, (-0.021858654, -0.021187821, -0.021052338, 0.005312909, 0.023380569, -0.340289268, -0.000216821))]
@@ -156,8 +176,9 @@ def load_attributes(gauge_id : str, df : pd.DataFrame) -> pd.DataFrame:
     data["silt_frac"] = [calc_soil_stats(attr, 301.024606, (-3.269011733, -2.264661005, -3.119504075, 0.453361295, 0.324598461, -23.46671657, -0.027876497))]
     data["clay_frac"] = [calc_soil_stats(attr, 632.9354852, (-6.360616113, -6.328565297, -5.609617704, -0.494360363, 0.17123592, -13.96685345, -0.029713411))]
     data["max_water_content"] = [calc_soil_stats(attr, -70.63807025, (0.703842654, 0.705460882, 0.706460986, 0.009564275, 0.01226467, 0.198735965, -0.000384683))]
-    data["carbonate_rocks_frac"] = [0.118742623] # average
     data["soil_porosity"] = [calc_soil_stats(attr, -7.621690463, (0.079823683, 0.080530022, 0.080618408, 0.001925408, 0, 0.244896375, -0.0001303))]
+    # Some stats are missing from NLDI so just take the average of CAMELS instead.
+    data["carbonate_rocks_frac"] = [0.118742623] # average
     data["frac_forest"] = [0.639539344] # average
     data["lai_max"] = [3.215969887] # average
     data["lai_diff"] = [2.448587614] # average
@@ -166,61 +187,126 @@ def load_attributes(gauge_id : str, df : pd.DataFrame) -> pd.DataFrame:
     data["gauge_id"] = [gauge_id]
     return pd.DataFrame(data).set_index("gauge_id")
 
-def load_gridmet(geometry : str, dates : Tuple[str, str]) -> pd.DataFrame:
-    var = ["pr", "pet", "srad", "tmmx", "tmmn", "sph"] # relative humidity or vpd for calculating vp? also check leap years
+def load_gridmet(geometry : Geometry, dates : Tuple[str, str]) -> pd.DataFrame:
+    """
+    Loads some GridMET data. Helper function for load_forcings.
+
+    Parameters
+    ----------
+    geometry : Geometry
+        The geometry of the area to load data for.
+    dates : Tuple[str, str]
+        The start and end dates to load data for, in "YYYY-MM-DD" format.
+    
+    Returns
+    -------
+    pd.DataFrame
+        The specified meteorological data, sourced from GridMET.
+    """
+    var = ["pr", "pet", "srad", "tmmx", "tmmn", "sph"]
     xarr = gridmet.get_bygeom(geometry, dates, variables=var, snow=True)
+    # Average data across spatial dimensions (keeping time) and convert to DataFrame.
     xarr = xarr.mean(dim=["lon", "lat"], skipna=True)
     df = xarr.to_dataframe()
+    # Format data to look more like CAMELS.
+    df.index.names = ["date"]
+    df["prcp"] = df["pr"]
+    df["tmax"] = df["tmmx"]
+    df["tmin"] = df["tmmn"]
+    df["vp"] = (101325 * df["sph"]) / (0.622 + 0.378 * df["sph"])
+    # Drop unnecessary columns.
+    df = df.drop(["spatial_ref", "pr", "tmmx", "tmmn", "sph"], axis=1)
     return df
 
 def load_forcings(gauge_id : str) -> Tuple[pd.DataFrame, int]:
-    # geometry for get_bygeom
+    """
+    Loads all meteorological data for a basin.
+
+    Parameters
+    ----------
+    gauge_id : str
+        The USGS gauge ID of the desired basin.
+    
+    Returns
+    -------
+    pd.DataFrame
+        A Pandas DataFrame containing labeled (to match CAMELS) forcings.
+    int
+        The area of the basin, in square meters.
+    """
+    # Set up COM ID and geometry from NLDI data.
     geometry = NLDI_DB.get_basins(gauge_id).geometry.iloc[0]
     com_id = int(NLDI_DB.getfeature_byid("nwissite", f"USGS-{gauge_id}")["comid"].iloc[0])
-    # area in square meters (nldi returns sq km)
+    # Calculate area in square meters for use in load_streamflow.
     area = int(NLDI_DB.get_characteristics(["TOT_BASIN_AREA"], com_id)["TOT_BASIN_AREA"].iloc[0] * 1000000)
-    if gauge_id == "01022500": # this one is weird for some reason
+    if gauge_id == "01022500": # this one is weird for some reason, other uncaught ones may exist
         area = 587675987
-    # because there's so much data, do it 5 years at a time
+    # Because there's so much data pre-spatial-average, load and reduce 5 years at a time.
     dfs = []
     for i in range(8):
         dates = (f"{1980+5*i}-01-01", f"{1984+5*i}-12-31")
         df = load_gridmet(geometry, dates)
         print(f"- forcings {i+1}/8 done")
         dfs.append(df)
-    # concatenate the segmented data
+    # Concatenate the segmented data.
     df = pd.concat(dfs, axis=0)
-    # format data to look more like camels
-    df.index.names = ["date"]
-    df["prcp"] = df["pr"]
-    df["tmax"] = df["tmmx"]
-    df["tmin"] = df["tmmn"]
-    df["vp"] = (101325 * df["sph"]) / (0.622 + 0.378 * df["sph"])
-    df = df.drop(["spatial_ref", "pr", "tmmx", "tmmn", "sph"], axis=1)
     return (df, area)
 
-def load_streamflow(gauge_id : str, area : int) -> pd.Series:
-    # open from online
-    parameters = "agencyCd=USGS&statCd=00003&parameterCd=00060&startDT=1980-01-01&endDT=2019-12-31"
-    usgs_url = f"https://waterservices.usgs.gov/nwis/dv?format=json&siteStatus=all&site={gauge_id}&{parameters}"
-    with request.urlopen(usgs_url) as f:
-        usgs = f.read().decode('utf-8')
+def online_json_to_df(url : str) -> pd.DataFrame:
+    """
+    Opens a URL that should point to a USGS JSON file, and converts it to
+    a DataFrame. Helper function for load_streamflow.
+
+    Parameters
+    ----------
+    url : str
+        The URL where the USGS JSON is located.
+    
+    Returns
+    -------
+    pd.DataFrame
+        A Pandas DataFrame containing the data stored in the JSON.
+    """
+    with request.urlopen(url) as f:
+        usgs = f.read().decode('utf-8') # unsure if the decode is necessary
         jdb = json.loads(usgs)
+    # The JSON contains a lot of metadata, get past it for the actual streamflow.
     timeseries = jdb["value"]["timeSeries"][0]["values"][0]["value"]
     df = pd.DataFrame.from_dict(timeseries)
+    return df
+
+def load_streamflow(gauge_id : str, area : int) -> pd.Series:
+    """
+    Loads all streamflow data for a basin.
+
+    Parameters
+    ----------
+    gauge_id : str
+        The USGS gauge ID of the desired basin.
+    
+    Returns
+    -------
+    pd.Series
+        A Pandas Series containing the streamflow, indexed by DateTime.
+    """
+    # Using urllib, we open an online file in JSON format to access USGS streamflow data. This may
+    # become deprecated, the USGS file is also not guaranteed to work for all basins (but it does for CAMELS)
+    parameters = "agencyCd=USGS&statCd=00003&parameterCd=00060&startDT=1980-01-01&endDT=2019-12-31"
+    usgs_url = f"https://waterservices.usgs.gov/nwis/dv?format=json&siteStatus=all&site={gauge_id}&{parameters}"
+    df = online_json_to_df(usgs_url)
+    # Format the data to resemble CAMELS.
     df["date"] = pd.to_datetime(df["dateTime"])
     df = df.set_index("date")
     df["QObs"] = pd.to_numeric(df["value"])
-    # replace invalid values with NaN
+    # Replace invalid values with NaN.
     df.loc[df["QObs"] < 0, "QObs"] = np.nan
-    # normalize discharge from cubic feet per second to mm per day
+    # Normalize discharge from cubic feet per second to mm per day.
     df["QObs"] = 28316846.592 * df["QObs"] * 86400 / (area * 10**6)
     return df["QObs"]
 
-# get basin list
+# From basin_list.txt, get a list of gauge IDs.
 with open(os.path.join(DATA_PATH, "basin_list.txt"), "r") as list_file:
     basin_list = list_file.read().splitlines()
-    # should be a list of gauge_id
 
 for basin in basin_list:
     print(f"loading data for basin {basin}...")
@@ -229,16 +315,16 @@ for basin in basin_list:
     if os.path.exists(attributes_path) and os.path.exists(timeseries_path):
         print("- already exists")
         continue
-    # get daymet data and process it for attributes
+    # Get GridMET data and process it, alongside NLDI, for attributes.
     forcings, area = load_forcings(basin)
     attributes = load_attributes(basin, forcings)
     attributes.to_csv(attributes_path)
     print(f"- attributes saved to {basin}.csv")
-    # re-index because daymet does weird stuff on leap years
+    # Re-index because GridMET doesn't include December 31 on leap years (why???).
     forcings = forcings.reindex(pd.date_range('01-01-1980', '12-31-2019'))
     forcings.index.names = ["date"]
+    # Drop unnecessary data and duplicate GridMET since nh-model wants three sources of forcings.
     forcings = forcings.drop(["snow", "pet"], axis=1)
-    # combine (duplicate) different forcings
     daymet_df = forcings.rename({"prcp": "prcp(mmperday)_daymet", "srad": "srad(Wperm2)_daymet", "tmax": "tmax(C)_daymet",
                        "tmin": "tmin(C)_daymet", "vp": "vp(Pa)_daymet"}, axis=1)
     maurer_df = forcings.rename({"prcp": "PRCP(mmperday)_maurer", "srad": "SRAD(Wperm2)_maurer", "tmax": "Tmax(C)_maurer",
@@ -247,10 +333,10 @@ for basin in basin_list:
                         "tmin": "Tmin(C)_nldas", "vp": "Vp(Pa)_nldas"}, axis=1)
     dfs = [daymet_df, maurer_df, nldas_df]
     df = pd.concat(dfs, axis=1)
-    # add streamflow data
+    # Add streamflow data.
     df["QObs(mmperd)"] = load_streamflow(basin, area)
     print("- streamflow done")
-    # save it to netcdf
+    # Save forcings to a NetCDF file.
     xarr = df.to_xarray()
     xarr.to_netcdf(timeseries_path)
     print(f"- forcings saved to {basin}.nc4")
